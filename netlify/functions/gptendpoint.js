ee2eb55cf879ded265509979d04ef18ac56c9aa2
Luke-Sheltraw <lukesheltraw@gmail.com>
2023-05-27 19:43:46 -0700

    429 error test

--- netlify/functions/gptendpoint.js
+++ netlify/functions/gptendpoint.js
@@ -3,28 +3,39 @@ const axios = require('axios');
 exports.handler = async function(event, context) {
   const prompt = event.queryStringParameters.prompt;
   
-  const response = await axios.post(
-    'https://api.openai.com/v1/chat/completions',
-    {
-	  model: 'gpt-3.5-turbo',
-      messages: [{role: 'user', content: prompt}],
-    },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-      }
-    }
-  );
-  
-  const chatGptResponse = response.data.choices[0].message.content;
+  try {
+	const response = await axios.post(
+		'https://api.openai.com/v1/chat/completions',
+		{
+		model: 'gpt-3.5-turbo',
+		messages: [{role: 'user', content: prompt}],
+		},
+		{
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+		}
+		}
+	);
+	
+	const chatGptResponse = response.data.choices[0].message.content;
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(chatGptResponse),
-	headers: {
-		'Access-Control-Allow-Origin': '*',
-		'Access-Control-Allow-Headers': 'Content-Type'
-	  }  
-  };
+	return {
+		statusCode: 200,
+		body: JSON.stringify(chatGptResponse),
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': 'Content-Type'
+		}  
+	};
+  } catch (error) {
+	return {
+		statusCode: 200,
+		body: `Error found, text: ${error}`,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': 'Content-Type'
+		}  
+	};
+  }
 }
